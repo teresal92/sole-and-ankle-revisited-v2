d8f78b2cb0ae579703944cae50dcf57a513fc143
Teresa Lew <teresalew92@gmail.com>
2025-12-10 15:03:13 -0800

    refactor: rename BREALPOINTS to BREAKPOINTS and update queries for consistency

--- src/constants.js
+++ src/constants.js
@@ -17,14 +17,14 @@ export const WEIGHTS = {
   bold: 800,
 };
 
-export const BREALPOINTS = {
-  phoneMax: 600,
-  tabletMax: 950,
-  laptopMax: 1300,
+export const BREAKPOINTS = {
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
 };
 
 export const QUERIES = {
-  phoneAndSmaller: `(max-width ${BREALPOINTS.phoneMax / 16}rem)`,
-  tabletAndSmaller: `(min-width ${BREALPOINTS.tabletMax / 16}rem)`,
-  laptopAndSmaller: `(min-width ${BREALPOINTS.laptopMax / 16}rem)`,
+  phoneAndSmaller: `(max-width ${BREAKPOINTS.phone / 16}rem)`,
+  tabletAndSmaller: `(max-width ${BREAKPOINTS.tablet / 16}rem)`,
+  laptopAndSmaller: `(min-width ${BREAKPOINTS.laptop / 16}rem)`,
 };
